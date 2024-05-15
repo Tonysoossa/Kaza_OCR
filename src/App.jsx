4d198e453543937e40e614f73e3cab5fe4499b7c
Tonysoossa <tony.servanin@gmail.com>
2024-05-15 16:54:19 +0200

    adding router in the work

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path
+  }
+])
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hell world!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
